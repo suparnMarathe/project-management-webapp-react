cf2c5dfebd80a27441abda03d920c89433517c03
suparnMarathe <suparnm98@gmail.com>
2023-11-27 12:41:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      {/* <NewProject /> */}
+      <NoProjectSelected/>
+    </main>
+  );
+}
+
+export default App;
